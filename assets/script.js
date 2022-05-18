134200d3431459b810dcb41115bb438733e55a10
Ted Glynn <will.glynn@gmail.com>
2022-05-18 11:36:13 -0600

    adding button call

--- assets/script.js
+++ assets/script.js
@@ -6,47 +6,52 @@
 // get multiple user choice on starch
 // addEventListener on "click" of "FindYourRecipe" to call API functions
 
-
-
 //define global variables
-var mealChoice = document.getElementById("option")
+var mealChoice = document.getElementById("option");
 var chefLevel;
 var proteinChoice;
 var vegChoice;
 var fruitChoice;
 var starchChoice;
-
-
+var searchBtn = document.getElementById("get-recipe-btn");
 
 function getTastyResults(event) {
-    event.preventDefault()
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
-            'X-RapidAPI-Key': 'af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac'
-        }
-    };
+  event.preventDefault();
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+      "X-RapidAPI-Key": "af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac",
+    },
+  };
 
-    fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+  fetch(
+    "https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
 
 //addEventListener to invoke getTastyResults func
 
 function amountConv() {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-            'X-RapidAPI-Key': 'af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac'
-        }
-    };
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": "af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac",
+    },
+  };
 
-    fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/convert?ingredientName=flour&targetUnit=grams&sourceUnit=cups&sourceAmount=2.5', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+  fetch(
+    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/convert?ingredientName=flour&targetUnit=grams&sourceUnit=cups&sourceAmount=2.5",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
+
+searchBtn.addEventListener("click", getTastyResults);
