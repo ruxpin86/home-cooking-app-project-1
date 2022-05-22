13508b2f811d137093b771a59f29ee59fb4ae83e
Ted Glynn <will.glynn@gmail.com>
2022-05-22 16:39:11 -0600

    got the search results layout to not be distorted, added the calorieNinja API call.

--- assets/script.js
+++ assets/script.js
@@ -17,59 +17,66 @@ userChoice = [
   // "breakfast",
   // "lunch",
   // "dinner",
-
   // "beef",
   // "poultry",
   // "fish",
-  "pork",
-
+  // "pork",
   // "tomatoes",
   // "onions",
   // "lettuce",
   // "carrots",
-
   // "berries",
   // "apples",
   // "bananas",
   // "citrus",
-
   // "grains",
   // "pasta",
   // "potatoes",
-  "bread",
+  // "bread",
 ];
 
 console.log(userChoice);
 
-//grab the user choice value from the class of choiceMeal
-// function getUserChoice() {
-//   mealChoice.textContent = " ";
-// }
-
-// mealChoice.addEventListener("click", function () {
-//get the value of the mealType to then be put into API call
-
-// getUserChoice();
-// console.log(getUserChoice);
-
-// function concatToSearch(event) {
-//   //get value from button click, then add onto search string
-
-// dynamically creates a list of results for su
+// dynamically creates a list of results for search results
 function setResultsList(res) {
   console.log(res);
   var resultEl = document.getElementById("search-results");
   resultEl.innerHTML = " ";
   res.hits.forEach((x) => {
     resultEl.innerHTML += `
-<div class="card">
+<div class="card col-sm-12 col-md-6 col-lg-4">
 <h3>${x.recipe.label}</h3>
-<img src="${x.recipe.image}" />
-<a href="${x.recipe.url}">View Recipe</a>
+<img class="card-img-top" alt="${x.recipe.label}" src="${x.recipe.image}"/>
+<div class="card-body">
+<a class="btn btn-outline-info choiceBtn" href="${x.recipe.url}">View Recipe</a>
+</div>
 </div>
 `;
   });
 }
+
+// get calorie information based on the name of the recipe
+function getCalorieNinja(query) {
+  //query corresponds to the 'label' argument passed on line 102
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "calorieninjas.p.rapidapi.com",
+      "X-RapidAPI-Key": "af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac",
+    },
+  };
+  // pasta%20alla%20gricia
+  fetch(
+    `https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${query}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    // try to call nutrition data as a variable to be assigned
+    .catch((err) => console.error(err));
+}
+
+// items[0].sodium_mg;
 
 function getEdamamRecipesList() {
   console.log("get recipe");
@@ -87,41 +94,20 @@ function getEdamamRecipesList() {
     options
   )
     .then((response) => response.json())
-    .then((response) => setResultsList(response))
-    .catch((err) => console.error(err));
-}
-
-function getTastyAutoResults(event) {
-  event.preventDefault();
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-      "X-RapidAPI-Key": "af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac",
-    },
-  };
-
-  fetch("https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=", options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-}
-
-function amountConv() {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      "X-RapidAPI-Key": "af7df4ee99mshd80fd154da3281bp1aeac7jsn5872eaa62dac",
-    },
-  };
-
-  fetch(
-    "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/convert?ingredientName=flour&targetUnit=grams&sourceUnit=cups&sourceAmount=2.5",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((response) => {
+      var recipes = response.hits;
+      recipes = recipes.map((item) => {
+        // var recipeLabel = [...item];
+        // console.log();
+        var tempItem = item;
+        var label = tempItem.recipe.label;
+        var inputLabel = getCalorieNinja(label);
+        console.log(inputLabel);
+        return tempItem;
+      });
+      // console.log(recipes);
+      setResultsList(response);
+    })
     .catch((err) => console.error(err));
 }
 
@@ -143,3 +129,6 @@ choiceBtns.forEach((btn) => {
 searchBtn.addEventListener("click", getEdamamRecipesList);
 
 //for ingredient types use foreach to loop through
+
+//var nutriFacts =
+//for (i=0 ;
